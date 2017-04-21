5d14e216de627830c34771ab098c54399eebb495
Yuya Matsumura <matsumurayuuya@yuya-no-MacBook-Pro.local>
2017-04-21 18:24:18 +0900

    initial commit

---
+++ pndic.R
@@ -0,0 +1,10 @@
+setwd("/Users/matsumurayuuya/Desktop/tmp/pndic")
+
+pndic_ja <- read.table("http://www.lr.pi.titech.ac.jp/~takamura/pubs/pn_ja.dic",
+                    sep = ":",
+                    col.names = c("term", "kana", "pos", "value"),
+                    colClasses = c("character", "character", "factor", "numeric"),
+                    fileEncoding = "Shift_JIS")
+
+save(pndic_ja, file = "pndic_ja.rda")
+write.csv(pndic_ja, "pndic_ja.csv")
